4f32ba9b51bbf2816117ecad64018d5f7df952b8
evgeniyPP <amenat12@mail.ru>
2020-03-24 20:53:37 +0300

    Eloquent ORM: relationships (1-M, M-M)

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
  */
 
+use App\Post;
+use App\Tag;
+use App\User;
+
 Route::get('/', 'PostController@index')->name('index');
 
 Route::group(['prefix' => '/post'], function () {
@@ -43,4 +47,37 @@ Route::group([], function () {
     Route::get('/signup', 'AuthController@signup_get')->name('auth.signup_get');
     Route::post('/signup', 'AuthController@signup_post')->name('auth.signup_post');
     Route::get('/logout', 'AuthController@logout')->name('auth.logout');
+});
+
+Route::get('/test', function () {
+    /* 1-MANY */
+    // User::create([
+    //     'name' => 'Влад',
+    //     'surname' => 'Кибардин',
+    //     'login' => 'kibaba',
+    //     'password' => password_hash('54321', \PASSWORD_DEFAULT)
+    // ]);
+
+    // Post::create([
+    //     'title' => 'Hello',
+    //     'content' => 'World',
+    //     'user_id' => '1'
+    // ]);
+
+    // $post = Post::find(2);
+    // $author = $post->user->name;
+
+    // dump($author); // Женя
+
+    /* MANY-MANY */
+
+    // Tag::create([
+    //     'name' => 'politics'
+    // ]);
+
+    $tag = Tag::find(1);
+    $post = Post::find(2);
+    $post->tags()->sync($tag);
+
+    return 'OK';
 });
